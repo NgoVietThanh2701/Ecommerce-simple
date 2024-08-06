7f3770c88816372ba463ed9db40e7db18083ec27
Ngo Viet Thanh <ngovietthanh2701@gmail.com>
2024-08-06 18:24:39 +0700

    update

--- smart_contract/scripts/deploy.ts
+++ smart_contract/scripts/deploy.ts
@@ -7,15 +7,15 @@ async function main() {
    const [deployer] = await ethers.getSigners();
    console.log('deploy from address: ', deployer.address);
 
-   // const AgiToken = await ethers.getContractFactory("AgriToken");
-   // const agriToken = await AgiToken.deploy();
-   // console.log("AgriToken address: ", agriToken.address);
-   // Config.setConfig(network + '.AgriToken', agriToken.address);
+   const LckToken = await ethers.getContractFactory("LuckToken");
+   const lckToken = await LckToken.deploy();
+   console.log("AgriToken address: ", lckToken.address);
+   Config.setConfig(network + '.LckToken', lckToken.address);
 
-   const Product = await ethers.getContractFactory("Products");
-   const product = await Product.deploy('0xcCBF9BcaAbeaE9d0F382695d6fFe31c39E533F17', '0xdB868486B732DEc5c42Dd3E1CCB77355465DC0Ac');
-   console.log("Product address: ", product.address);
-   Config.setConfig(network + '.Product', product.address);
+   // const Product = await ethers.getContractFactory("Products");
+   // const product = await Product.deploy('0xcCBF9BcaAbeaE9d0F382695d6fFe31c39E533F17', '0xdB868486B732DEc5c42Dd3E1CCB77355465DC0Ac');
+   // console.log("Product address: ", product.address);
+   // Config.setConfig(network + '.Product', product.address);
 
    await Config.updateConfig();
 }
